ca494839651362b587a14ac9839005e231b04270
22-2301-351 Vincent Louise Obeso <vincentlouise.obeso@cit.edu>
2024-11-02 16:29:56 +0800

    3 - payload limit updated

--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -14,34 +14,33 @@ const CreatePost = () => {
 	const {data: authUser} = useQuery({ queryKey: ["authUser"]});
 	const queryClient = useQueryClient();
 
-	const {mutate: createPost, isPending, isError, error} = useMutation({
-		mutationFn: async ({text, img}) => {
+	const { mutate: createPost, isPending, isError, error } = useMutation({
+		mutationFn: async ({ text, img }) => {
 			try {
 				const res = await fetch("/api/posts/create", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
 					},
-					body: JSON.stringify({text, img})
-				})
-				const data = res.json();
-
-				if(!res.ok) {
+					body: JSON.stringify({ text, img }),
+				});
+				const data = await res.json();
+				if (!res.ok) {
 					throw new Error(data.error || "Something went wrong");
 				}
-
 				return data;
 			} catch (error) {
 				throw new Error(error);
 			}
 		},
+
 		onSuccess: () => {
 			setText("");
 			setImg(null);
 			toast.success("Post created successfully");
-			queryClient.invalidateQueries({queryKey: ["posts"]});
-		}
-	})
+			queryClient.invalidateQueries({ queryKey: ["posts"] });
+		},
+	});
 
 
 	const handleSubmit = (e) => {
